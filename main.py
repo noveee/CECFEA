d14456327a5a162fa0e59cbde400752d9f1f8cb8
November <thepythonmc@gmail.com>
2023-05-09 12:44:50 -0400

    For demo

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import functions
 
 path = input("What's the name of the spreadsheet: ") + ".xlsx"
 sheet = functions.getWorkbook(path)
-print(functions.getAverageForColumn(sheet, 15)) 
+
+for i in range(11, 19):
+    print(functions.getAverageForColumn(sheet, i)) 
 
 # Loop through each column and find average of each
 # Get average of overall evaluation
